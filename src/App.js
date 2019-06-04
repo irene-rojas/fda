f33e5c4a22ec87a923f77a62cfe1dadc1efce287
Irene R <irene@smallwondermedia.com>
2019-06-04 18:31:11 -0400

    api key undefined

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+    const [adverse, setAdverse] = ([]);
+
+    useEffect(() => {
+        axios.get(`https://api.fda.gov/drug/event.json?api_key=${process.env.REACT_APP_FDA_API}&search=receivedate:[20040101+TO+20081231]&limit=5`)
+        .then(res => {
+            setAdverse(res.data);
+        })
+    })
+
+    // const adverseEvent = () => {
+    //     axios.get(`https://api.fda.gov/drug/event.json?api_key=${process.env.REACT_APP_FDA_API}&search=receivedate:[20040101+TO+20081231]&limit=5`)
+    //     .then(res => {
+    //         setAdverse(res.data);
+    //         console.log(res.data);
+    //     });
+    // };
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <button>Search</button>
+
   );
 }
 
