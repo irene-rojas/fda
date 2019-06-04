83c71e78413ed35109e91ec372b7b6fef05cc5d9
Irene R <irene@smallwondermedia.com>
2019-06-04 18:57:51 -0400

    adverse reactions basic api

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import axios from 'axios';
 
 function App() {
 
-    const [adverse, setAdverse] = ([]);
+    const [adverse, setAdverse] = useState([]);
 
-    useEffect(() => {
-        axios.get(`https://api.fda.gov/drug/event.json?api_key=${process.env.REACT_APP_FDA_API}&search=receivedate:[20040101+TO+20081231]&limit=5`)
+    const adverseEffect = () => {
+        axios.get(`https://api.fda.gov/drug/event.json?api_key=${process.env.REACT_APP_FDA_API}&search=receivedate:%5B20040101+TO+20081231%5D&limit=1`)
         .then(res => {
             setAdverse(res.data);
+            console.log(res.data);
         })
-    })
-
-    // const adverseEvent = () => {
-    //     axios.get(`https://api.fda.gov/drug/event.json?api_key=${process.env.REACT_APP_FDA_API}&search=receivedate:[20040101+TO+20081231]&limit=5`)
-    //     .then(res => {
-    //         setAdverse(res.data);
-    //         console.log(res.data);
-    //     });
-    // };
-
+    };
 
 
   return (
 
-    <button>Search</button>
+    <div>
+        <button onClick={() => adverseEffect()}>Search</button>
+    </div>
 
   );
 }
