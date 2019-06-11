6777fae1eee14892b2f0cf49e94f2d89d837afb9
Irene R <irene@smallwondermedia.com>
2019-06-10 22:15:48 -0400

    APPR API

--- src/App.js
+++ src/App.js
@@ -4,13 +4,29 @@ import axios from 'axios';
 
 function App() {
 
-    const [adverse, setAdverse] = useState([]);
+    // const [adverse, setAdverse] = useState([]);
+    const [approved, setApproved] = useState([]);
+    const [cleared, setCleared] = useState(false);
 
-    const adverseEffect = () => {
-        axios.get(`https://api.fda.gov/drug/event.json?api_key=${process.env.REACT_APP_FDA_API}&search=patient.drug.openfda.pharm_class_epc:"nonsteroidal+anti-inflammatory+drug"&limit=1`)
+    // const adverseEffect = () => {
+    //     axios.get(`https://api.fda.gov/drug/event.json?api_key=${process.env.REACT_APP_FDA_API}&search=patient.drug.openfda.pharm_class_epc:"nonsteroidal+anti-inflammatory+drug"&limit=1`)
+    //     .then(res => {
+    //         setAdverse(res.data.results);
+    //         console.log(res.data.results);
+    //     })
+    // };
+
+    const approvedCleared = () => {
+        axios.get(`https://api.fda.gov/device/pma.json?api_key=${process.env.REACT_APP_FDA_API}&search=decision_code:APPR&limit=5`)
         .then(res => {
-            setAdverse(res.data.results);
+            setApproved(res.data.results);
             console.log(res.data.results);
+            console.log(res.data.results[1].decision_date);
+            console.log(res.data.results[1].advisory_committee_description);
+            console.log(res.data.results[1].generic_name);
+            console.log(res.data.results[1].trade_name);
+            console.log(res.data.results[1].openfda.device_class);
+
         })
     };
 
@@ -18,8 +34,12 @@ function App() {
   return (
 
     <div>
-        <div>Disclaimer from openFDA: Do not rely on openFDA to make decisions regarding medical care. While we make every effort to ensure that data is accurate, you should assume all results are unvalidated.</div>
-        <button onClick={() => adverseEffect()}>Search</button>
+        <div>
+            Disclaimer from openFDA: Do not rely on openFDA to make decisions regarding medical care. While we make every effort to ensure that data is accurate, you should assume all results are unvalidated.
+        </div>
+        {/* <button onClick={() => adverseEffect()}>Search</button> */}
+        <button onClick={() => approvedCleared()}>Search</button>
+
     </div>
 
   );
